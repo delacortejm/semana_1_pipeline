22c07d48a9d1ad62c926d7ba261044429bf8b6ea
delacortejm <delacortejm@gmail.com>
2025-07-21 21:51:25 -0300

    creación función load_data

--- semana_1_pipeline/main.py
+++ semana_1_pipeline/main.py
@@ -1,17 +1,9 @@
-from pipeline.load import load_csv
+from pipeline.load import load_csv, load_data
 from pipeline.save import save_csv
 
 # Cargar datos (ruta corregida)
-df = load_csv("data/input.csv")
+df = load_data("data/input.csv")
 print(df.head())
 
 # Guardar datos procesados
 save_csv(df, "data/output/clean.csv")
-
-# from pipeline.load import load_csv
-# from pipeline.save import save_csv
-
-# df = load_csv("data/input.csv")
-# print(df.head())
-
-# save_csv(df, "data/output/clean.csv")
--- semana_1_pipeline/pipeline/load.py
+++ semana_1_pipeline/pipeline/load.py
@@ -11,3 +11,11 @@ def load_csv(path: str) -> pd.DataFrame:
         pd.DataFrame: Datos cargados.
     """
     return pd.read_csv(path)
+
+def load_data(path: str) -> pd.DataFrame:
+    if path.endswith(".csv"):
+        return pd.read_csv(path)
+    elif path.endswith(".parquet"):
+        return pd.read_parquet(path)
+    else:
+        raise ValueError("Formato no soportado")
